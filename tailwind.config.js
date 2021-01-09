fc41429896086fe6f2c089520586395069311676
Jay McMullen <jay@safewill.com>
2021-01-09 19:50:51 +1000

    Add explainer video

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./src/**/*.ejs', './src/**/*.md'],
   theme: {
     extend: {},
+    aspectRatio: {
+      none: 0,
+      square: [1, 1],
+      '16/9': [16, 9],
+      '4/3': [4, 3],
+      '21/9': [21, 9],
+    },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    aspectRatio: ['responsive'],
+  },
+  plugins: [
+    require('tailwindcss-responsive-embed'),
+    require('tailwindcss-aspect-ratio'),
+  ],
 };
